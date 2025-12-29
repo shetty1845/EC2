cf1cc2323e2898189826c9f02e18040912716764
Tharuna Sree Golla <121326990+tharunasree-19@users.noreply.github.com>
2025-12-29 13:40:39 +0530

    app.py

--- app.py
+++ app.py
@@ -625,4 +625,6 @@ def server_error(e):
     return render_template('500.html', is_logged_in=is_logged_in()), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+
