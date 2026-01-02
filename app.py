5ed01adfc54a5a3014c314bd9f430651a83fffed
Tharuna Sree Golla <121326990+tharunasree-19@users.noreply.github.com>
2026-01-02 09:47:15 +0530

    app.py

--- app.py
+++ app.py
@@ -672,4 +672,5 @@ def server_error(e):
     return render_template('500.html', is_logged_in=is_logged_in()), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
